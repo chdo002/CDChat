cb1f790c04a5dca1c4a70b1e385628481f5d7614
chdo <chdo@chdodeMacBook-Pro.local>
2017-10-25 16:22:50 +0800

    Initial commit

---
+++ Example/CDChatList/CDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDAppDelegate.h
+//  CDChatList
+//
+//  Created by chdo002 on 10/25/2017.
+//  Copyright (c) 2017 chdo002. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/CDChatList/CDViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDViewController.h
+//  CDChatList
+//
+//  Created by chdo002 on 10/25/2017.
+//  Copyright (c) 2017 chdo002. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CDViewController : UIViewController
+
+@end
