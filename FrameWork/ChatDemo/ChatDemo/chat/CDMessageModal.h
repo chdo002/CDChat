22e19e4015cbd5671ae69f6d40ac043982e3611c
chdo002 <1107661983@qq.com>
2017-12-21 11:33:58 +0800

    update

---
+++ FrameWork/ChatDemo/ChatDemo/chat/CDMessageModal.h
@@ -0,0 +1,18 @@
+//
+//  CDMessageModal.h
+//  CDChatList_Example
+//
+//  Created by chdo on 2017/10/26.
+//  Copyright © 2017年 chdo002. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AATChatList.h"
+
+
+@interface CDMessageModal : NSObject<MessageModalProtocal>
+
++(CDMessageModal *)initWithDic:(NSDictionary *)dic;
+
+
+@end
---
+++ FrameWork/ChatDemo/ChatDemo/input/InputBoxViewController.h
@@ -0,0 +1,14 @@
+//
+//  InputBoxViewController.h
+//  CDChatList_Example
+//
+//  Created by chdo on 2017/12/8.
+//  Copyright © 2017年 chdo002. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AATChatList.h"
+#import "AATUtility.h"
+@interface InputBoxViewController : UIViewController
+
+@end
